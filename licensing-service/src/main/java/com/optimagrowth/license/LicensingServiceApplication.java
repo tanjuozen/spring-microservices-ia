e0a1724e9439a8bd119d520953edcde1b4e59df9
Tanzhu Ozen <tanjuozen90@gmail.com>
2020-10-25 15:47:46 +0100

    - adding organization-service and eureka-server.yml - licensing and organization services are now Eureka clients in the this setup

--- licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -2,6 +2,7 @@ package com.optimagrowth.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -12,6 +13,7 @@ import java.util.Locale;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class LicensingServiceApplication {
 
     public static void main(String[] args) {
